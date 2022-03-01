28a4bc2b183009025da90f7a68b0455dc35e3b6e
Lok <lb.tamang77@gmail.com>
2022-03-01 11:08:17 -0700

    'change'

--- reverseArray.js
+++ reverseArray.js
@@ -24,16 +24,30 @@
 // console.log(reverse(['a', 'b', 'c']));
 
 // in place method
-function reverse(arr) {
-    for(let i = 0; i < (arr.length / 2); i++){
-        const first = i;
-        const last = [arr.length - 1 - i]
-        let temp = arr[first]
-        arr[i] = arr[last]
-        arr[last] = temp
-    }
-    return arr
-}
+// function reverse(arr) {
+//     for(let i = 0; i < (arr.length / 2); i++){
+//         const first = i;
+//         const last = [arr.length - 1 - i]
+//         let temp = arr[first]
+//         arr[i] = arr[last]
+//         arr[last] = temp
+//     }
+//     return arr
+// }
 
-let result = reverse([1, 2, 3, 4, 5, 6, 7]);
-console.log(result);
+// let result = reverse([1, 2, 3, 4, 5, 6, 7]);
+// console.log(result);
+
+// ***********************Reverse Array Recursive method Sol 1 **********************
+let result = [];
+function reverseArray(arr) {
+  if (arr.length === 0) return [];
+  if (!Array.isArray(arr)) return "Expected array data type!";
+  result.push(arr.splice(arr.length - 1, 1)[0]);
+  reverseArray(arr);
+  return result;
+}
+const final = reverseArray([1, 2, 3, 4, 5, 6]);
+// const final = reverseArray([]);
+// const final = reverseArray("hi");
+console.log(final);
--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -1,35 +1,38 @@
 //Linked list data structure
 
-class Node{
-    constructor(value){
-        this.value = value
-        this.next = 0
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = 0;
+  }
 }
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.length = 0
-    }
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    push(value){
-        var newNode = new Node(value);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else{
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+  push(value) {
+    var newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push('lok');
-list.push('tamang');
+list.push("lok");
+list.push("tamang");
 list.push(123);
-console.log(list.tail.next);
+list.push("el paso");
+console.log(list.head);
+console.log(list.head.value);
+console.log(list.head.next);
